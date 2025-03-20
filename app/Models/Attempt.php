029e911f5808f4c627a4733ce17faddddb3eeb36
NAKAJIMA Junko <k132528@kansai-u.ac.jp>
2025-03-20 09:02:16 +0000

    relation

--- app/Models/Attempt.php
+++ app/Models/Attempt.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Attempt extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function feedbacks()
+    {
+        return $this->hasMany(Feedback::class);
+    }
+
+    public function word()
+    {
+        return $this->belongsTo(Word::class);
+    }
 }
--- app/Models/Studyhistory.php
+++ app/Models/Studyhistory.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Studyhistory extends Model
 {
     use HasFactory;
+
+    public function feedbacks()
+    {
+        return $this->hasMany(Feedback::class);
+    }
+
+    public function deck()
+    {
+        return $this->belongsTo(Deck::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
