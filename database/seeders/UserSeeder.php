99933456748430411440817325c4ff4a6eddac90
NAKAJIMA Junko <k132528@kansai-u.ac.jp>
2025-03-31 07:33:15 +0000

    password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use DateTime;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -19,7 +20,7 @@ class UserSeeder extends Seeder
             'name' => '先生3',
             'email' => 'nakasu0330@gmail.com',
             'email_verified_at' => new DateTime(),
-            'password' => 'laraveltest',
+            'password'  => Hash::make('laraveltest'),
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
      ]);
